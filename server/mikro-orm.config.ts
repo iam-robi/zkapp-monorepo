dca40a6598f4381f96fb221eb10e2f4df74ca7bf
robi <robin.sarfati@realconsultingdata.com>
2022-11-22 14:14:12 +0100

    :sparkles: update migration for prod

--- server/mikro-orm.config.ts
+++ server/mikro-orm.config.ts
@@ -3,31 +3,29 @@ import { MikroOrmModuleOptions as Options } from '@mikro-orm/nestjs';
 
 import { LoadStrategy } from '@mikro-orm/core';
 
-
 const config: Options = {
-    entities: ['dist/**/*.entity.js'],
-    entitiesTs: ['src/**/*.entity.ts'],
-    // entities: [Registration],
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dbName: process.env.DB_DATABASE,
-    // port: Number(process.env.PG_PORT),
-    type: 'postgresql',
-    debug: true,
-    loadStrategy: LoadStrategy.JOINED,
-    migrations: {
-        path: 'dist/src/migrations',
-        pathTs: 'src/migrations',
-    },
-    seeder: {
-        defaultSeeder: 'DatabaseSeeder',
-        //generated seeder must be moved from dist to src folder (error)
-        path: 'dist/src/seeders',
-        // pathTs is not changing output
-        // pathTs: './src/seeders'
-    },
-
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  // entities: [Registration],
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dbName: process.env.DB_DATABASE,
+  // port: Number(process.env.PG_PORT),
+  type: 'postgresql',
+  debug: true,
+  loadStrategy: LoadStrategy.JOINED,
+  migrations: {
+    path: 'dist/src/migrations',
+    pathTs: 'src/migrations',
+  },
+  seeder: {
+    defaultSeeder: 'DatabaseSeeder',
+    //generated seeder must be moved from dist to src folder (error)
+    path: 'dist/src/seeders',
+    // pathTs is not changing output
+    // pathTs: './src/seeders'
+  },
 };
 
 export default config;
