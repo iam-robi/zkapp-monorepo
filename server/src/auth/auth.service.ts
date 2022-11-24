7949f5c40b99b979e8ed7b9839196b34d91c90ab
robi <robin.sarfati@realconsultingdata.com>
2022-11-24 13:46:19 +0100

    cleaning

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -9,38 +9,32 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    let user = await this.userService.findByAddress(username);
-    if(!user) {
-      this.userService.save({
-        address: username
-      })
-      user = await this.userService.findByAddress(username);
-    }
-    return user;
-    // if (user && user.password === pass) {
-    //   const { password, ...result } = user;
-    //   return result;
+  // async validateUser(username: string, pass: string): Promise<any> {
+  //   let user = await this.userService.findByAddress(username);
+  //   if(!user) {
+  //     this.userService.save({
+  //       address: username
+  //     })
+  //     user = await this.userService.findByAddress(username);
+  //   }
+  //   return user;
+  //   // if (user && user.password === pass) {
+  //   //   const { password, ...result } = user;
+  //   //   return result;
+  //   // }
+  //   // return null;
+  // }
+
+  async validateAddress(req: Request): Promise<any> {
+    // let user = await this.userService.findByAddress(address);
+    // if(!user) {
+    //   this.userService.save({
+    //     address: address
+    //   })
+    //   user = await this.userService.findByAddress(address);
     // }
-    // return null;
+    // return user;
   }
 
-  async validateAddress(address): Promise<any> {
-    let user = await this.userService.findByAddress(address);
-    if(!user) {
-      this.userService.save({
-        address: address
-      })
-      user = await this.userService.findByAddress(address);
-    }
-    return user;
-  }
 
-  async login(user: any) {
-    const payload = { username: user.email, sub: user.id };
-    console.log(this.jwtService)
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
-  }
 }
