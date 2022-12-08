5584e5eecb6da6f001946dc8adc9a302d0771f3e
robi <robin.sarfati@realconsultingdata.com>
2022-12-08 18:11:36 +0100

    add paths

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,8 @@ const ssx = new SSXServer({
       //https://github.com/DefinitelyTyped/DefinitelyTyped/blob/a24d35afe48f7fb702e7617b983ddca1904ba36b/types/express-session/index.d.ts#L52
       sessionOptions: {
         cookie: {
-          domain: "mina-oracle-ui.dev.lensuscloud.com"
+          domain: "lensuscloud.com",
+          sameSite: "lax"
         }
       }
     }
